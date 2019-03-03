596feb09dcf30b5e7aa49550e7da533e34c2cdc4
LiXing <2212640110@qq.com>
2019-03-03 13:08:17 +0800

    添加项目的AdminServer监控,和邮件告警功能.

---
+++ adminserver/src/main/java/com/lx/eimsadminserver/EimsadminserverApplication.java
@@ -0,0 +1,13 @@
+package com.lx.eimsadminserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EimsadminserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EimsadminserverApplication.class, args);
+    }
+
+}
